9fa23ac23945f8ae09ec6957aeb56ca473a230be
Sylvia Engmann <sylvia.engmann@ashesi.edu.gh>
2017-01-18 08:33:05 +0000

    added filesfor the canteen application

---
+++ setting1.php
@@ -0,0 +1,11 @@
+<?php
+//define("DB_HOST","localhost");
+//define("DB_USERNAME","sylvia.engmann");
+//define("DB_PASSWORD","9f1d6eedd1ff7ccc");
+//define("DB_NAME","dbms_sylvia.engmann");
+
+define("DB_HOST","localhost");
+define("DB_USERNAME","root");
+define("DB_PASSWORD","");
+define("DB_NAME","applied_db");
+?>
