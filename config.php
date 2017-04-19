d5c3de9e68296792a598baeecc47a8d5e7a9c00d
Sylvia Engmann <sylvia.engmann@ashesi.edu.gh>
2017-04-19 09:01:34 +0000

    changed server name

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-	$site_root = "/~sylvia.engmann/applied/AshCanteenII/";
+	$site_root = "http://cs.ashesi.edu.gh/~sylvia.engmann/applied/AshCanteenII/";
 	$image_folder = "http://cs.ashesi.edu.gh/~gifty.mate-kole/finalproject/uploads/";
 	//$site_root = "/AshCanteenII/";
 	$debug = false;
